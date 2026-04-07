aa1efafc17da80612b6548d1d0bba04a59a6fd4f
tikoh-station <mannyassuncao@hotmail.com>
2026-04-07 22:50:14 +0200

    Add clayL.tcl test example

---
+++ run.sh
@@ -0,0 +1,16 @@
+export SUNLOGGER_ERROR_FILENAME="sundials.error.log"
+export SUNLOGGER_WARNING_FILENAME="sundials.warning.log"
+export SUNLOGGER_INFO_FILENAME="sundials.info.log"
+export SUNLOGGER_DEBUG_FILENAME="sundials.debug.log"
+
+export PARFLOW_DIR="${PWD}/parflow_install"
+
+xsplit=1
+ysplit=1
+nprocs=1 # must be a square number
+ncells=100 # 100 x 100 x 240 cells
+
+tclsh clayL.tcl ${xsplit} ${ysplit} ${nprocs} ${ncells}
+
+${PARFLOW_DIR}/bin/parflow clayL_${xsplit}_${ysplit}_${nprocs}_${ncells} \
+  > out.txt 2> error.txt
